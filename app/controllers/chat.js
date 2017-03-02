d17f2d1da314eb8f5eeab73a241fb9d5c5bfa1f5
soonfy <soonfy@163.com>
2017-03-02 23:50:22 +0800

    chat

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -3,24 +3,82 @@
  */
 const socket = require('socket.io');
 
+var getTime=function(){
+  var date = new Date();
+  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
+
+var getColor=function(){
+  var colors = ['aliceblue','antiquewhite','aqua','aquamarine','pink','red','green',
+                'orange','blue','blueviolet','brown','burlywood','cadetblue'];
+  return colors[Math.round(Math.random() * 10000 % colors.length)];
+}
+
 const chat = (server) => {
   let io = socket.listen(server);
 
-  io.on('connection', (socket) => {
-    console.log('[server] client conn.');
-    socket.emit('open');
+  io.on('connection', function (socket) {
+    console.log(socket.request.headers);
+    socket.emit('open'); //通知客户端已连接
 
-    socket.on('msg', (msg) => {
-      console.log('[server]', msg);
-      socket.emit('system', `[emit] ${msg}`);
-      socket.broadcast.emit('system', `[broadcast] ${msg}`);
-    })
+    // 打印握手信息
+    // console.log(socket.handshake);
 
-    socket.on('disconnect', () => {
-      socket.emit('system', `[emit] disconnect`);
-      socket.broadcast.emit('system', `[broadcast] disconnect`);
-    })
+    // 构造客户端对象
+    var client = {
+      socket: socket,
+      name: false,
+      color: getColor()
+    }
+
+    // 对message事件的监听
+    socket.on('message', function (msg) {
+      var obj = {
+        time: getTime(),
+        color: client.color
+      };
+
+      // 判断是不是第一次连接，以第一条消息作为用户名
+      if (!client.name) {
+        client.name = msg;
+        obj['text'] = client.name;
+        obj['author'] = 'System';
+        obj['type'] = 'welcome';
+        console.log(client.name + ' login');
+
+        //返回欢迎语
+        socket.emit('system', obj);
+        //广播新用户已登陆
+        socket.broadcast.emit('system', obj);
+      } else {
+
+        //如果不是第一次的连接，正常的聊天消息
+        obj['text'] = msg;
+        obj['author'] = client.name;
+        obj['type'] = 'message';
+        console.log(client.name + ' say: ' + msg);
+
+        // 返回消息（可以省略）
+        socket.emit('message', obj);
+        // 广播向其他用户发消息
+        socket.broadcast.emit('message', obj);
+      }
+    });
+
+    //监听出退事件
+    socket.on('disconnect', function () {
+      var obj = {
+        time: getTime(),
+        color: client.color,
+        author: 'System',
+        text: client.name,
+        type: 'disconnect'
+      };
+
+      // 广播用户已退出
+      socket.broadcast.emit('system', obj);
+      console.log(client.name + 'Disconnect');
+    });
   })
 }
-
-module.exports = chat;
+    module.exports = chat;
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,21 +1,50 @@
 $(() => {
   console.log('chat page document over.');
+  let user = '<%= user %>';
+  console.log(user);
 
-  let socket = io.connect('localhost://3000');
-  socket.on('open', () => {
-    console.log('[client] connect server suc.');
+  let list = $('#list');
+  let hist = $('#hist');
+  let cont = $('#cont');
+
+  //建立websocket连接
+  socket = io.connect('http://localhost:3000');
+  //收到server的连接确认
+  socket.on('open', function () {
+    console.log('[client] connect suc.');
+  });
+
+  //监听system事件，判断welcome或者disconnect，打印系统消息信息
+  socket.on('system', function (json) {
+    console.log(json);
+    let p = '';
+    if (json.type === 'welcome') {
+      // if (myName == json.text) status.text(myName + ': ').css('color', json.color);
+      p = '<p style="background:' + json.color + '">system  @ ' + json.time + ' : Welcome ' + json.text + '</p>';
+    } else if (json.type == 'disconnect') {
+      p = '<p style="background:' + json.color + '">system  @ ' + json.time + ' : Bye ' + json.text + '</p>';
+    }
+    hist.prepend(p);
+  });
+
+  //监听message事件，打印消息信息
+  socket.on('message', function (json) {
+    console.log(json);
+    var p = '<p><span style="color:' + json.color + ';">' + json.author + '</span> @ ' + json.time + ' : ' + json.text + '</p>';
+    hist.prepend(p);
+  });
+
+  //通过“回车”提交聊天信息
+  cont.keydown((e) => {
+    if (e.keyCode === 13) {
+      var msg = cont.val();
+      if (!msg) {
+        return;
+      } else {
+        socket.send(msg);
+        cont.val('');
+      }
+    }
   })
 
-  socket.on('system', (msg) => {
-    console.log('[client] receive system', msg);
-  })
-
-  socket.on('msg', (msg) => {
-    console.log('[client] receive msg', msg);
-  })
-
-  $('button').click(() => {
-    console.log('click button');
-    socket.send('client send msg.');
-  })
 })
