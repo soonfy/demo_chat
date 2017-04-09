071b499427b3f0753c0cb6732d65e03562696da8
soonfy <soonfy@163.com>
2017-04-09 23:16:07 +0800

    chat events

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -8,6 +8,12 @@ const moment = require('moment');
 let chat = (server) => {
   let io = socket.listen(server);
 
+  let STATUS = {
+    HALL: 'hall',
+    ROOM: 'room',
+    USER: 'user'
+  };
+
   let rooms = {
     'Node.js': [],
     'JavaScript': [],
@@ -16,48 +22,58 @@ let chat = (server) => {
     '自然语言处理': [],
     '大数据': []
   };
-  let users = [];
+  let allusers = [];
 
   io.on('connection', (socket) => {
 
-    socket.on('online', (user) => {
-      let index = 1;
-      while (users.includes(user)) {
+    // 用户登录聊天室
+    socket.on('online', (message) => {
+      let {
+        user
+      } = message, index = 1;
+      while (allusers.includes(user)) {
         user = index === 1 ? user : user.slice(0, user.lastIndexOf('-'));
         user = [user, index].join('-');
         index++;
       }
       socket.user = user;
-      users.push(user);
-
+      allusers.push(user);
       let data = {
-        type: '系统消息',
-        user: '系统消息',
-        text: user,
-        users,
-        rooms,
-        date: moment().format('YYYY-MM-DD HH:mm:ss')
+        type: '系统消息', // 消息类型
+        user: user, // 消息来源
+        text: user, // 消息内容
+        allusers, // 用户信息
+        rooms, // 房间信息
+        date: moment().format('YYYY-MM-DD HH:mm:ss') // 时间戳
       };
-      socket.emit('online', data);
+      console.log(`${user} online.`);
+      io.emit('online', data);
     });
 
+    // 用户加入房间
     socket.on('join', (message) => {
       let {
         room,
         user
       } = message;
+      socket.room = room;
       if (!rooms[room].includes(user)) {
         rooms[room].push(user);
       }
       let data = {
         type: '系统消息',
-        user: '系统消息',
-        room,
+        user: user,
+        room, // 房间名称
         text: user,
-        users: rooms[room],
+        room,
+        users: rooms[room], // 房间用户信息
+        allusers,
+        rooms,
         date: moment().format('YYYY-MM-DD HH:mm:ss')
       };
       socket.join(room, () => {
+        console.log(`${user} join ${room}.`);
+        socket.broadcast.emit('flush', data);
         io.to(room).emit('join', data);
       });
     });
@@ -67,46 +83,76 @@ let chat = (server) => {
         room,
         user
       } = message;
+      socket.room = null;
       if (rooms[room].includes(user)) {
         rooms[room].splice(rooms[room].indexOf(user), 1);
       }
       let data = {
         type: '系统消息',
-        user: '系统消息',
+        user: user,
         room,
         text: user,
-        users,
+        room,
+        users: rooms[room],
+        allusers,
         rooms,
         date: moment().format('YYYY-MM-DD HH:mm:ss')
       };
-      socket.join(room, () => {
+      socket.leave(room, () => {
+        console.log(`${user} leave ${room}.`);
+        socket.broadcast.emit('flush', data);
+        socket.emit('leave', data);
         io.to(room).emit('leave', data);
       });
     });
 
     socket.on('disconnect', () => {
-      if (users.includes(socket.user)) {
-        users.splice(users.indexOf(socket.user), 1);
-        let data = {
-          type: '系统消息',
-          user: '系统消息',
-          text: socket.user,
-          users,
-          date: moment().format('YYYY-MM-DD HH:mm:ss')
-        };
-        socket.broadcast.emit('offline', data);
+      let user = socket.user,
+        room = socket.room;
+      if (allusers && allusers.includes(user)) {
+        allusers.splice(allusers.indexOf(user), 1);
       }
+      if (rooms[room] && rooms[room].includes(user)) {
+        rooms[room].splice(rooms[room].indexOf(user), 1);
+      }
+      let data = {
+        type: '系统消息',
+        user: user,
+        text: user,
+        room,
+        users: rooms[room],
+        allusers,
+        rooms,
+        date: moment().format('YYYY-MM-DD HH:mm:ss')
+      };
+      socket.broadcast.emit('offline', data);
     });
 
     socket.on('chat', (message) => {
+      let {user, text, room, chatTo} = message;
       let data = {
         type: '用户消息',
-        user: message.user,
-        text: message.text,
-        users,
+        user,
+        text,
+        room,
+        chatTo,
         date: moment().format('YYYY-MM-DD HH:mm:ss')
       };
-      io.emit('chat', data);
+      if (chatTo.type === STATUS.HALL) {
+        io.emit('chat', data);
+      } else if (chatTo.type === STATUS.ROOM) {
+        io.to(room).emit('chat', data);
+      } else {
+        let socketid;
+        let sockets = io.sockets.sockets;
+        for (var id in sockets) {
+          if (sockets[id].user === chatTo.user) {
+            socketid = id;
+          }
+        }
+        io.to(socketid).emit('chat', data);
+        socket.emit('chat', data);
+      }
     });
   })
 }
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,70 +1,133 @@
 $(() => {
   console.log('chat page document over.');
 
-  function scrollToBottom() {
-    $('#content').scrollTop($('#content')[0].scrollHeight);
+  let user = Cookies.get('uname').trim();
+  let uroom;
+  let STATUS = {
+    HALL: 'hall',
+    ROOM: 'room',
+    USER: 'user'
+  };
+  let status = STATUS.HALL;
+  let chatTo = {
+    type: STATUS.HALL
   };
 
+  // let socket = io('http://localhost:3000');
+  let socket = io();
+  let timeout = 1000 * 60 * 10;
+  let last = new Date() - timeout;
+
+  function scrollToBottom() {
+    $('#content').scrollTop($('#content')[0].scrollHeight);
+  }
+
   function flushRoomList(message) {
-    let {rooms, users} = message;
+    let {
+      rooms,
+      allusers
+    } = message;
     let roomsArr = Object.keys(rooms);
-    $('#list').empty().append(`<div title="房间列表(${roomsArr.length}) - ${users.length}">房间列表(${roomsArr.length}) - ${users.length}</div>`);
+    $('#list').empty().append(`<div title="房间列表(${roomsArr.length}) - ${allusers.length}">房间列表(${roomsArr.length}) - ${allusers.length}</div>`);
     roomsArr.map((_room) => {
       $('#list').append(`<p data-type="room" name="${_room}" title="房间 ${_room}, 双击进入">${_room} - ${rooms[_room].length}</p>`)
     })
-  }
-
-  function flushUserList(message) {
-    let {users, room} = message;
-    $('#list').empty().append(`<div title="${room} - ${users.length}">${room} - ${users.length} </div>`);
-    $('#list').append(`<div data-type="leave" name="${room}" title="双击离开房间">离开房间</div>`);
-    users.map((_user) => {
-      $('#list').append(`<p data-type="user" title="用户 ${_user}, 双击私信">${_user}</p>`)
-    })
-  }
-
-  let user = Cookies.get('uname').trim();
-  // let socket = io('http://localhost:3000');
-  let socket = io();
-  let timeout = 1000 * 60 * 10
-  let last = new Date() - timeout;
-
-  socket.emit('online', user);
-  socket.on('online', (message) => {
-    let msg = $('<div class="msg">'),
-      temp = $('<p>'),
-      time = new Date();
-    msg.append($('<p>').text(message.user));
-    temp.append($('<span>').text(`你已进入聊天室。`));
-    if (time - last > timeout) {
-      temp.append('<br>');
-      temp.append($('<span>').text(message.date));
-      last = time;
-    }
-    msg.append(temp);
-    $('#content').append(msg);
-    scrollToBottom();
-    flushRoomList(message);
-
     $('[data-type="room"]').dblclick((e) => {
       let room = $(e.target).attr('name');
       let data = {
         room,
         user
-      }
+      };
+      status = STATUS.ROOM;
+      uroom = room;
+      chatTo.type = STATUS.ROOM;
       socket.emit('join', data);
     })
+  }
+
+  function flushUserList(message) {
+    let {
+      users,
+      room
+    } = message;
+    $('#list').empty().append(`<div data-type="room" title="${room} - ${users.length}, 双击回到房间模式。">${room} - ${users.length} </div>`);
+    $('#list').append(`<div data-type="leave" name="${room}" title="双击离开房间">离开房间</div>`);
+    users.map((_user) => {
+      $('#list').append(`<p data-type="user" name="${_user}" title="用户 ${_user}, 双击私信模式。">${_user}</p>`)
+    })
+    $('[data-type="leave"]').dblclick((e) => {
+      let room = $(e.target).attr('name');
+      let data = {
+        room,
+        user
+      };
+      status = STATUS.HALL;
+      chatTo.type = STATUS.HALL;
+      uroom = null;
+      socket.emit('leave', data);
+    })
+    $('[data-type="room"]').dblclick((e) => {
+      let _user = $(e.target).attr('name');
+      chatTo.type = STATUS.ROOM;
+    })
+    $('[data-type="user"]').dblclick((e) => {
+      let name = $(e.target).attr('name');
+      let text = $('#message').text();
+      if (chatTo.type === STATUS.USER) {
+        text = text.replace(/\[.+\]:?/, '');
+      }
+      $('#message').text(`[say to ${name}]`);
+      // $('#message').text(`[say to ${name}] ${text}`);
+      chatTo.user = name;
+      chatTo.type = STATUS.USER;
+    })
+  }
+
+  // 用户登录聊天室
+  let data = {
+    user
+  };
+  socket.emit('online', data);
+
+  socket.on('online', (message) => {
+    let msg = $('<div class="msg">'),
+      temp = $('<p>'),
+      time = new Date();
+    msg.append($('<p>').text(message.type));
+    if (status === STATUS.HALL) {
+      if (message.user === user) {
+        temp.append($('<span>').text(`你已进入聊天室。`));
+      } else {
+        temp.append($('<span>').text(`欢迎新用户 - ${message.text} -`));
+      }
+      if (time - last > timeout) {
+        temp.append('<br>');
+        temp.append($('<span>').text(message.date));
+        last = time;
+      }
+      msg.append(temp);
+      $('#content').append(msg);
+      scrollToBottom();
+      flushRoomList(message);
+    }
   });
 
+  socket.on('flush', (message) => {
+    if (status === STATUS.HALL) {
+      flushRoomList(message);
+    }
+  });
+
+  // 用户进入房间
   socket.on('join', (message) => {
     let msg = $('<div class="msg">'),
       temp = $('<p>'),
       time = new Date();
-    msg.append($('<p>').text(message.user));
-    if (message.text === user) {
-      temp.append($('<span>').text(`你已进入房间 - ${message.room}。`));
+    msg.append($('<p>').text(message.type));
+    if (message.user === user) {
+      temp.append($('<span>').text(`你已进入房间 ${message.room}。`));
     } else {
-      temp.append($('<span>').text(`欢迎新用户 - ${message.text} -`));
+      temp.append($('<span>').text(`用户 - ${message.text} - 已进入房间。`));
     }
     if (time - last > timeout) {
       temp.append('<br>');
@@ -75,23 +138,18 @@ $(() => {
     $('#content').append(msg);
     scrollToBottom();
     flushUserList(message);
-
-    $('[data-type="leave"]').dblclick((e) => {
-      let room = $(e.target).attr('name');
-      let data = {
-        room,
-        user
-      }
-      socket.emit('leave', data);
-    })
   });
 
   socket.on('leave', (message) => {
     let msg = $('<div class="msg">'),
       temp = $('<p>'),
       time = new Date();
-    msg.append($('<p>').text(message.user));
-    temp.append($('<span>').text(`你已退出房间 - ${message.room}。`));
+    msg.append($('<p>').text(message.type));
+    if (message.user === user) {
+      temp.append($('<span>').text(`你已退出房间 - ${message.room}。`));
+    } else {
+      temp.append($('<span>').text(`用户 - ${message.text} - 已退出房间。`));
+    }
     if (time - last > timeout) {
       temp.append('<br>');
       temp.append($('<span>').text(message.date));
@@ -100,16 +158,11 @@ $(() => {
     msg.append(temp);
     $('#content').append(msg);
     scrollToBottom();
-    flushRoomList(message);
-
-    $('[data-type="room"]').dblclick((e) => {
-      let room = $(e.target).attr('name');
-      let data = {
-        room,
-        user
-      }
-      socket.emit('join', data);
-    })
+    if (message.user === user) {
+      flushRoomList(message);
+    } else {
+      flushUserList(message);
+    }
   });
 
 
@@ -117,17 +170,29 @@ $(() => {
     let msg = $('<div class="msg">'),
       temp = $('<p>'),
       time = new Date();
-    msg.append($('<p>').text(message.user));
-    temp.append($('<span>').text(`用户 - ${message.text} - 离开了聊天室。`));
-    if (time - last > timeout) {
-      temp.append('<br>');
-      temp.append($('<span>').text(message.date));
-      last = time;
+    msg.append($('<p>').text(message.type));
+    if (status === STATUS.HALL) {
+      temp.append($('<span>').text(`用户 - ${message.text} - 离开了聊天室。`));
+      if (time - last > timeout) {
+        temp.append('<br>');
+        temp.append($('<span>').text(message.date));
+        last = time;
+      }
+      msg.append(temp);
+      $('#content').append(msg);
+      flushRoomList(message);
+    } else if (uroom === message.room) {
+      temp.append($('<span>').text(`用户 - ${message.text} - 离开了房间。`));
+      if (time - last > timeout) {
+        temp.append('<br>');
+        temp.append($('<span>').text(message.date));
+        last = time;
+      }
+      msg.append(temp);
+      $('#content').append(msg);
+      flushUserList(message);
     }
-    msg.append(temp);
-    $('#content').append(msg);
     scrollToBottom();
-    flushUserList(message);
   });
 
   $('#message').keyup((e) => {
@@ -138,7 +203,9 @@ $(() => {
       }
       let data = {
         user,
-        text
+        text,
+        room: uroom,
+        chatTo
       };
       socket.emit('chat', data);
       $('#message').text('');
@@ -151,29 +218,33 @@ $(() => {
     }
     let data = {
       user,
-      text
+      text,
+      room: uroom,
+      chatTo
     };
     socket.emit('chat', data);
     $('#message').text('');
   });
   socket.on('chat', function (message) {
-    let msg = $('<div class="msg">'),
-      temp = $('<p>'),
-      time = new Date();
-    if (message.user === user) {
-      temp = $('<p class="right">');
-      msg.append($('<p class="right">').text(message.user));
-    } else {
-      msg.append($('<p>').text(message.user));
+    if (message.chatTo.type !== STATUS.HALL || status === message.chatTo.type) {
+      let msg = $('<div class="msg">'),
+        temp = $('<p>'),
+        time = new Date();
+      if (message.user === user) {
+        temp = $('<p class="right">');
+        msg.append($('<p class="right">').text(message.user));
+      } else {
+        msg.append($('<p>').text(message.user));
+      }
+      temp.append($('<span>').text(message.text));
+      if (time - last > timeout) {
+        temp.append('<br>');
+        temp.append($('<span>').text(message.date));
+        last = time;
+      }
+      msg.append(temp);
+      $('#content').append(msg);
+      scrollToBottom();
     }
-    temp.append($('<span>').text(message.text));
-    if (time - last > timeout) {
-      temp.append('<br>');
-      temp.append($('<span>').text(message.date));
-      last = time;
-    }
-    msg.append(temp);
-    $('#content').append(msg);
-    scrollToBottom();
   });
 })
